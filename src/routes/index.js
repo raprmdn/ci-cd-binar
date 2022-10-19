1e8f08f8e018e55296773974a6e0718b3685eb2e
raprmdn <rafiputraramadhan14@gmail.com>
2022-10-19 19:49:10 +0700

    init

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { StatusCodes: status } = require('http-status-codes');
+const { apiResponse } = require('../utils/apiResponse.utils');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    return res.status(status.OK).json(
+        apiResponse( status.OK, 'OK', 'API Routes')
+    );
+});
+
+module.exports = router;
